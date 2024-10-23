f9805039ef636ae1c27d88e055b2ad527064dee8
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2024-10-23 10:49:36 -0700

    quiz1partB

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import turtle
+from turtle import *
 
 # YOUR CODE HERE
+def draw_square():
+    for i in range (4):
+        forward(100)
+        right(90)
+
+draw_square()
+penup()
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(50)
+pendown()
+
+right(45)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+
+penup()
+right(135)
+forward(100)
+right(125)
 
 turtle.exitonclick()
